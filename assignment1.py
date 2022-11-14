3319d484b02a7cd6527ae387dce8cc261b36e659
AljazR <aljoz.rakovec@gmail.com>
2022-11-14 15:41:30 +0100

    Fixed coordinates

--- assignment1.py
+++ assignment1.py
@@ -1,6 +1,7 @@
 import pygad
 import numpy as np
 import time
+import math
 from mazes import *
 
 # Conversion from array of strings to integer numpy matrix
@@ -9,18 +10,20 @@ def convert(mazeChars):
     return np.array([[char2num[char] for char in line] for line in mazeChars], dtype=np.int8)
 
 # Choose the maze
-original_new = maze2
-maze = convert(original_new)
+original_maze = maze2
+maze = convert(original_maze)
 
-# Get the coordinates of 'S' and 'E'
-start = list(zip(*np.where(maze == 2)))[0]
-end = list(zip(*np.where(maze == 3)))[0]
+# Get the coordinates of 'S' and 'E': (x, y)
+start_temp = list(zip(*np.where(maze == 2)))[0]
+end_temp = list(zip(*np.where(maze == 3)))[0]
+start = (start_temp[1], start_temp[0])
+end = (end_temp[1], end_temp[0])
 
 # Set parameters for GA
 num_generations = 1000 # Number of generations.
 num_parents_mating = 7 # Number of solutions to be selected as parents in the mating pool.
 sol_per_pop = 50 # Number of solutions in the population.
-gene_space = [0, 1, 2, 3] # [U, D, L, R]
+gene_space = [0, 1, 2, 3, 4] # [up, down, left, right, stop]
 num_genes = np.where(maze == 1)[0].size # Set to number of dots in the maze.
 
 def fitness_func(solution, solution_idx):
@@ -29,28 +32,27 @@ def fitness_func(solution, solution_idx):
 
 last_fitness = 0
 def on_generation(ga_instance):
-    global last_fitness
-    # print("Generation = {generation}".format(generation=ga_instance.generations_completed))
-    # print("Fitness    = {fitness}".format(fitness=ga_instance.best_solution()[1]))
-    # print("Change     = {change}".format(change=ga_instance.best_solution()[1] - last_fitness))
     generation = ga_instance.generations_completed
-    fitness = ga_instance.best_solution()[1]
-    change = ga_instance.best_solution()[1] - last_fitness
-    print("Generation: %-10d Fitness: %-10d Change: %-10d" % (generation, fitness, change))
+    if generation % 1 == 0:
+        global last_fitness
+        fitness = ga_instance.best_solution()[1]
+        change = ga_instance.best_solution()[1] - last_fitness
+        print("Generation: %-10d Fitness: %-10d Change: %-10d" % (generation, fitness, change))
     last_fitness = ga_instance.best_solution()[1]
 
 def on_stop(ga_instance, fitness):
     solution = np.asarray(ga_instance.best_solution()[0], dtype=int).tolist()
-    # solution = [2,2,2,0,0,3,3,3,0,0] # Example of solution for maze2
-    
+    # solution = [2,2,2,0,0,3,3,3,0,0,4,4,4,4,4] # Example of solution for maze2
+
     num2char = ['U','D','L','R']
     index_end = 0
-    solution = [5] + solution # So 'X' starts on 'S'
+    solution = [4] + solution # So 'X' starts on 'S'
     x = start[0]
     y = start[1]
+    width, height = maze.shape 
 
     print()
-    [print(i) for i in original_new]
+    [print(i) for i in original_maze]
     time.sleep(0.7)
 
     # Set x or y
@@ -58,40 +60,38 @@ def on_stop(ga_instance, fitness):
         index_end += 1
 
         if i == 0:
-            x -= 1
-        elif i == 1:
-            x += 1
-        elif i == 2:
             y -= 1
-        elif i == 3:
+        elif i == 1:
             y += 1
+        elif i == 2:
+            x -= 1
+        elif i == 3:
+            x += 1
         
         # Erasing previous maze
-        for j in range(maze.shape[1]):
+        for j in range(height):
             print('\033[1A', end='\x1b[2K') # line_up='\033[1A' and line_clear='\x1b[2K'
 
         # Write out the new maze
-        if x < maze.shape[0] and y < maze.shape[1]:
-            new_maze = np.copy(original_new)
-            new_maze[x] = new_maze[x][:y] + 'X' + new_maze[x][y+1:]
+        if (0 <= y and y < height) and (0 <= x and x < width):
+            new_maze = np.copy(original_maze
+        )
+            new_maze[y] = new_maze[y][:x] + 'X' + new_maze[y][x+1:]
             [print(i) for i in new_maze]
-            
-            if maze[x,y] == 3:
-                solution = solution[1:index_end]
-                print('\nSolution: ', end='')
-                [print(num2char[i], end ="") for i in solution]
-                print('\n')
-                return
         else:
-            [print(i) for i in original_new]
+            [print(i) for i in original_maze
+        ]
         
-        time.sleep(0.5)
+        time.sleep(0.2)
 
     # Print solution
-    solution = solution[1:]
     print('\nSolution:', end='')
-    [print(num2char[i], end ="") for i in solution]
+    for i in solution:
+        if i != 4:
+            print(num2char[i], end ="")
     print('\n')
+    print(solution[1:])
+
     return
 
 # Creating an instance of the GA class inside the ga module.
