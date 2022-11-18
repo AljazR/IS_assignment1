1f5a06ca520e3fb2d3ed964d31f38a19e146931e
Klemen262 <klemen.bogataj2000@gmail.com>
2022-11-18 15:48:41 +0100

    initial population and mutation

--- assignment1.py
+++ assignment1.py
@@ -3,34 +3,36 @@ import numpy as np
 import time
 import math
 from mazes import *
+import random
 
 # Conversion from array of strings to integer numpy matrix
 char2num = {"#": 0, ".": 1, "S": 2, "E": 3}
 def convert(mazeChars):
-    return np.array([[char2num[char] for char in line] for line in mazeChars], dtype=np.int8)
+    return np.array([[char2num[char] for char in line] for line in mazeChars], dtype=np.int32)
+
+move_letters = ["U", "D", "L", "R"]
 
 # Choose the maze
-original_maze = maze7
+original_maze = maze5
 maze = convert(original_maze)
 
 # Get the coordinates of 'S' and 'E': (row, column)
 start = list(zip(*np.where(maze == 2)))[0]
 end = list(zip(*np.where(maze == 3)))[0]
+print(start, end)
 
 # Moves before hitting wall or to the end
 def valid_solution_part(moves: np.ndarray, maze: np.ndarray):
     a, b = start
-    moves_sum = np.array([moves == 0, moves == 1, moves == 2, moves == 3]).cumsum(axis=1)
-    indexes1 = a - moves_sum[0,:] + moves_sum[1,:]
-    indexes2 = b - moves_sum[2,:] + moves_sum[3,:]
-    out_of_maze1 = np.nonzero(np.logical_or(indexes1 < 0, indexes1 >= maze.shape[0]))[0]
-    out_of_maze2 = np.nonzero(np.logical_or(indexes2 < 0, indexes2 >= maze.shape[1]))[0]
+    rows, cols = indexes_visited(moves, a, b)
+    out_of_maze1 = np.nonzero(np.logical_or(rows < 0, rows >= maze.shape[0]))[0]
+    out_of_maze2 = np.nonzero(np.logical_or(cols < 0, cols >= maze.shape[1]))[0]
     n_valid_moves = moves.size
     if out_of_maze1.size > 0 :
         n_valid_moves = min(out_of_maze1[0], n_valid_moves)
     if out_of_maze2.size > 0:
         n_valid_moves = min(out_of_maze2[0], n_valid_moves)
-    field_values = maze[indexes1[:n_valid_moves], indexes2[:n_valid_moves]]
+    field_values = maze[rows[:n_valid_moves], cols[:n_valid_moves]]
     walls = np.nonzero(field_values == 0)[0]
     if walls.size > 0:
         n_valid_moves = walls[0] # index of first move, that hit the wall
@@ -40,7 +42,7 @@ def valid_solution_part(moves: np.ndarray, maze: np.ndarray):
         finished = True
         n_valid_moves = end[0] + 1
     different_fields = np.zeros_like(maze)
-    different_fields[indexes1[:n_valid_moves], indexes2[:n_valid_moves]] = 1
+    different_fields[rows[:n_valid_moves], cols[:n_valid_moves]] = 1
     dots_travelled = different_fields.sum()
     return moves[:n_valid_moves], finished, dots_travelled
 
@@ -51,6 +53,37 @@ def fitness_func_klemen(solution, solution_idx):
         return 1000 - n_moves
     else:
         return dots_travelled - n_moves / 1000
+
+def fitness_func_klemen2(solution, solution_idx):
+    moves, finished, dots_travelled = valid_solution_part(solution, maze)
+
+    # indexes of rows and columns
+    rows, cols = indexes_visited(solution, start[0], start[1])
+
+    # indexes of moves, not rows an columns
+    out_of_maze_rows = np.nonzero(np.logical_or(rows < 0, rows >= maze.shape[0]))[0]
+    out_of_maze_cols = np.nonzero(np.logical_or(cols < 0, cols >= maze.shape[1]))[0]
+    out = np.unique(np.concatenate((out_of_maze_cols, out_of_maze_rows)))
+    in_maze = np.arange(solution.shape[0])
+    in_maze[out] = -1
+    in_maze = in_maze[np.nonzero(in_maze != -1)]
+    fields = maze[rows[in_maze], cols[in_maze]]
+    walls = np.sum(fields == 0)
+    dots = np.sum(fields == 1)
+    fields_travelled = np.zeros_like(maze)
+    fields_travelled[rows[in_maze], cols[in_maze]] = 1
+    different_dots = ((maze == 1) * fields_travelled).sum()
+    #print("".join([move_letters[int(m)] for m in solution]))
+    #print(rows)
+    #print(cols)
+    #print(fields, walls, dots, different_dots, out.size)
+    #return
+    if finished:
+        return 1000000 - moves.size
+    else:
+        return out.size * (-10000) + walls * (-100) + different_dots
+
+
 
 # Define fitness function
 def fitness_func_aljaz(solution, solution_idx):
@@ -113,11 +146,110 @@ def crossover_func(parents, offspring_size, ga_instance):
         id_parent += 1
 
     return np.array(offspring)
-    
+
+# Initial population with all solutions arriving to end, but going through walls
+def initial_to_end_through_walls(sol_per_pop, num_genes, start, end, maze_shape):
+    col, row = start
+    population = np.zeros((sol_per_pop, num_genes), dtype=np.int32)
+    for i in range(sol_per_pop):
+        distance_row = end[0] - start[0]
+        distance_col = end[1] - start[1]
+        free_moves = num_genes - abs(distance_col) - abs(distance_row)
+        if distance_row > 0:
+            up = free_moves // 4
+            down = free_moves // 4 + distance_row
+        else:
+            up = free_moves // 4 - distance_row
+            down = free_moves // 4
+        if distance_col > 0:
+            left = free_moves // 4
+            right = free_moves // 4 + distance_col
+        else:
+            left = free_moves // 4 - distance_col
+            right = free_moves // 4
+        moves = np.zeros(num_genes)
+        moves[:up + down + left + right] = np.concatenate((np.zeros(up), 
+            np.ones(down), np.ones(left) * 2, np.ones(right) * 3))
+        np.random.shuffle(moves[:up + down + left + right])
+        for j in range(free_moves % 4):
+            moves[-1-j] = random.randint(0, 3)
+        population[i,:] = moves
+    return population
+
+# calculates row and column indexes after all moves
+def indexes_visited(moves, start_col, start_row):
+    moves_sum = np.array([moves == 0, moves == 1, moves == 2, moves == 3]).cumsum(axis=1)
+    rows = start_col - moves_sum[0,:] + moves_sum[1,:]
+    cols = start_row - moves_sum[2,:] + moves_sum[3,:]
+    return rows, cols
+
+"""
+        for j in range(num_genes):
+            # 0 1 2 3 ~ U D L R
+            axis = random.randint(0, 1) # 0: U/D, 1: L/R
+            if distance > num_genes - j:
+                direction = random.randint(0, 1) # 0: U/L, 1: D/R
+            else:
+                direction = 0 
+
+            move = axis * 2 + direction
+            col += (-1) ** (1 - direction) * (1 - axis)
+            row += (-1) ** (1 - direction) * axis
+            distance = abs(end[0] - col) + abs(end[1] - row)
+            population[i,j] = move
+"""
+
+
+
+
+
+# Randomly select a different move
+def random_new(move):
+    return (move + random.randint(1, 3)) % 4
+
+# Randomly selects a new perpendicular move
+def random_perpendicular(move):
+    return (move + (-1) ** random.randint(1, 2)) % 4
 
 # Define mutatoion function
-def mutation_func():
-    a = 1
+def mutation_func_two_adjacent_genes(offspring, ga_instance):
+    for chromosome_idx in range(offspring.shape[0]):
+        offspring[chromosome_idx, :] = mutate_two_adjacent_genes(offspring[chromosome_idx, :])
+    return offspring
+
+# Mutate two adjacent genese like so, that moves behind mutation stay on tha same path
+def mutate_two_adjacent_genes(chromosome: np.ndarray):
+    valid_part, _, _ = valid_solution_part(chromosome, maze)
+    mutation_location = random.randint(0, chromosome.size - 2)
+    #mutation_location = random.randint(0, valid_part.size)
+    #mutation_location = valid_part.size
+    gene1, gene2 = chromosome[mutation_location], chromosome[mutation_location + 1]
+
+    # eliminates a back and forth move, adds 2 new moves to the end
+    if ((gene1 + 2) % 4) == gene2:
+        chromosome[mutation_location:-2] = chromosome[mutation_location + 2:]
+        chromosome[-2] = random.randint(0, 4)
+        chromosome[-1] = random.randint(0, 4)
+
+    # "Rotates" a corner: LU to UL, DL to LD ...
+    elif gene1 % 2 != gene2 % 2:
+        chromosome[mutation_location] = gene2
+        chromosome[mutation_location + 1] = gene1
+    
+    # Goes around: LL -> ULLD / DLLU, UU -> RUUL / LUUR ...
+    else:
+        # To close to the end, randomly choose new move
+        if mutation_location > chromosome.size - 4:
+            chromosome[mutation_location] = random_new(gene1)
+        # Goes around, deleting last two moves
+        else:
+            r_p = random_perpendicular(gene1)
+            # Shifts moves to right
+            chromosome[mutation_location + 4 :] = chromosome[mutation_location + 2 : -2]
+            # Inserts moves to go around
+            chromosome[mutation_location : mutation_location + 4] = np.array((r_p, gene1, gene2, (r_p + 2) % 4))
+    return chromosome
+
 
 # Runs after every generation of GA
 last_fitness = 0
@@ -182,19 +314,24 @@ def on_stop(ga_instance, fitness):
         print(num2char[i], end ="")
     print('\n')
 
+
+population = initial_to_end_through_walls(50, np.where(maze == 1)[0].size * 2, start, end, maze.shape)
+print(population[0])
+
 # Creating an instance of the GA class inside the ga module.
 ga_instance = pygad.GA(num_generations=2000,
                        num_parents_mating=10, 
-                       sol_per_pop=50, 
+                       sol_per_pop=50,
+                       initial_population=population,
                        num_genes=np.where(maze == 1)[0].size * 2,
                        gene_space=[0, 1, 2, 3],
                        keep_elitism=5,
-                       random_seed=2,
-                       stop_criteria="saturate_500", # stop evolution, if the fitness does not change for 100 consecutive generations.
-                       crossover_type="uniform",
+                       #random_seed=2,
+                       stop_criteria="saturate_100", # stop evolution, if the fitness does not change for 100 consecutive generations.
+                       crossover_type=None,
                     #    crossover_type=crossover_func,
-                    #    mutation_type=mutation_func,
-                       fitness_func=fitness_func_klemen,
+                       mutation_type=mutation_func_two_adjacent_genes,
+                       fitness_func=fitness_func_klemen2,
                        on_generation=on_generation,
                        on_stop=on_stop)
 
